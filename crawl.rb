a5bff0d07d6fd40da8e24f779e3320b1e1b01b80
John Smith <john@ishared.org>
2012-02-27 22:40:40 +0200

    Updated to newer version of crawl

--- crawl.rb
+++ crawl.rb
@@ -6,15 +6,29 @@ class Crawl
     '+' => :spellbook,':' => :spellbook,'}' => :misc,'$' => :gold}
   KNOWN_OBJECTS = {:ground => ['.'], :walls => ['#'], :creatures => ('a'..'z').to_a,
     :hero => ['@'], :items => ITEM_TYPES.keys}
+  PLAYER_MAIN_DETAILS = {:name => 'Player', :species => 'Human',
+    :background => 'Fighter'}
+  PLAYER_SECOND_DETAILS = {:weapon => 'short sword'}
 
-  def initialize(name, species, occupation)
-    @cmd_arguements = "-name #{name} -race #{species} -class #{occupation}"
+  def initialize(details)
+    @cmd_arguements = ''
+    PLAYER_MAIN_DETAILS.each do |key,value|
+      new_value = (details[key] || value)
+      @cmd_arguements += " -#{key} #{new_value}"
+    end
+
+    @cmd_arguements += ' -extra-opt-first'
+    PLAYER_SECOND_DETAILS.each do |key,value|
+      new_value = (details[key] || value)
+      @cmd_arguements += " #{key}='#{new_value}'"
+    end
   end
 
   def run_program(&block)
     @data = {}
     @interface = TerminalEmulator.new
-    @interface.run_program('crawl ' + @cmd_arguements) do
+    @interface.run_program('crawl' + @cmd_arguements) do
+      @interface.take_screen_shot
       parse_screen
       block.call(@data)
     end
@@ -57,13 +71,17 @@ class Crawl
   end
 
   def parse_player_stats(screen)
+    return unless screen[0]
+    return if screen[0].strip.scan(' the ') == []
     info = {}
     stats = screen.map do |line|
-      line[39..-1] if line and line.length > 39
+      line[37..-1] if line and line.length > 39
     end
     info[:title] = (stats[0] || '').strip
     info[:title] = '' if info[:title].scan(' the ') == []
     info[:species] = (stats[1] || '').strip
+    magic = (stats[3] || '').split(' ')[1]
+    info[:magic_remaining] = magic.split('/')[0].to_i if magic
     info
   end
 
--- crawl_tiles.rb
+++ crawl_tiles.rb
@@ -1,18 +1,30 @@
 require_relative 'crawl'
 
-@warrior = Crawl.new('John','Human','Fighter')
+NAME = 'Joe'
+SPECIES = 'Mummy'
+BACKGROUND = 'Summoner'
+WEAPON = 'short sword'
+PLAYER_DETAILS = {:name => NAME, :species => SPECIES,
+  :background => BACKGROUND, :weapon => WEAPON}
+
+@warrior = Crawl.new(PLAYER_DETAILS)
 @warrior.run_program do |data|
   @last_message = '' unless @last_message
   @warrior.take_screen_shot unless @last_message == data[:message]
   p data[:message] if data[:message] != @last_message
   @last_message = data[:message]
 
-  if data[:message].scan('Which weapon?') != []
-    @warrior.press_buttons('a')
-  elsif data[:stats][:title] != ''
-    @warrior.display_screen
-    @warrior.press_buttons(['S','Y'])
+  @warrior.display_screen
+  if data[:stats] != nil and data[:stats][:magic_remaining] != nil
+    @warrior.press_buttons(['S','y'])
     @warrior.stop_program
+  end
+  if data[:stats] != nil
+    if data[:stats][:magic_remaining] > 0 and data[:objects][:creatures].length > 0
+      @warrior.press_buttons(['z','a'])
+    else
+      @warrior.press_buttons(['o'])
+    end
   end
 end
 
